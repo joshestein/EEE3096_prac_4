1a32a380b85af748c5a16336b31039bb53721068
Josh Stein <josh.e.stein@gmail.com>
2018-10-04 15:29:36 +0200

    Secure insecure callback

--- main.py
+++ main.py
@@ -20,8 +20,14 @@ pot_channel = 5
 
 prev_voltage = 0
 start = True
+secure = False
 begin = 0.0
 end = 0.0
+user_times = []
+user_directions = []
+
+code_times = [200, 200, 1000] #in ms
+code_directions = ["L", "L", "R"]
 
 def start_stop_callback(channel):
     global start, begin
@@ -30,6 +36,13 @@ def start_stop_callback(channel):
     else:
         start = True
         begin = time.time()
+
+def secure_insecure_callback(channel):
+    global secure
+    if secure:
+        secure = False
+    else:
+        secure = True
 
 def setup():
     """Setup button, MCP and pot."""
@@ -49,6 +62,7 @@ def setup():
 
     GPIO.add_event_detect(start_stop_btn, GPIO.FALLING, callback=start_stop_callback, bouncetime=300)
     begin = timer()
+    GPIO.add_event_detect(secure_btn, GPIO.FALLING, callback=secure_insecure_callback, bouncetime=300)
 
 def read_pot(pot_voltage):
     if pot_voltage > prev_voltage:
@@ -58,10 +72,27 @@ def read_pot(pot_voltage):
         prev_voltage = pot_voltage
         return "R"
 
+def check_times(secure):
+    global user_times, code_times
+    if secure == False:
+        #TODO: sort user times, code times
+    for i in range(0, len(user_times)):
+        if user_times[i] != code_times[i]:
+            return False 
+    return True
+
+def check_positions(secure):
+    global user_positions, code_positions
+    if secure == False:
+        #TODO: sort user positions, code_positions
+    for i in range(0, len(user_positions)):
+        if user_positions[i] != user_positions[i]:
+            return False 
+    return True
+
 def main():
-    global start, begin, end
+    global start, begin, end, secure
     setup()
-    print("Start: "+str(start))
 
     while start:
         pot_voltage = mcp.read_adc(pot_channel)
@@ -71,6 +102,11 @@ def main():
 
         if (end-begin) > 2:
             start = False
+    
+    if (check_times(secure) and check_positions(secure)):
+        #TODO: success!
+    else:
+        #TODO: waa waa waaaaa    
 
 if __name__ == "__main__":
     main()
