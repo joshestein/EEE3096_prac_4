d3d320a679b9297ee76532ca2ba312cd0639ee65
Josh Stein <josh.e.stein@gmail.com>
2018-10-04 13:42:51 +0200

    Main function (#3)

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ secure_btn = 23
 
 # Setup
 #--------------------
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(secure_btn, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(secure_btn, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+def main():
+    setup()
+    #TODO
+
+if name == "__main__":
+    main()
 
