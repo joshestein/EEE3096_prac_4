cffbf7715473848d595243611824304456c70345
Josh Stein <josh.e.stein@gmail.com>
2018-10-04 14:06:03 +0200

    Setup ADC (#4)

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import RPi.GPIO as GPIO
+import Adafruit_MCP3008
 
 # Constants
 #--------------------
-
 secure_btn = 23
+CLK = 21
+MOSI = 20
+MISO = 19
+CS = 26
 
-#--------------------
-
-# Setup
-#--------------------
 def setup():
+    """Setup button, MCP and pot."""
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(secure_btn, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+    mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 def main():
     setup()
