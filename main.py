331c327024a66fff7dae5aaa8995a25c9058983a
Josh Stein <josh.e.stein@gmail.com>
2018-10-18 12:31:15 +0200

    Better start functionality

--- main.py
+++ main.py
@@ -31,17 +31,22 @@ code_times = [300, 300, 300] #in ms
 code_directions = ["L", "L", "R"]
 
 def reset():
-    global begin, user_times, user_directions
+    global begin, user_times, user_directions, start, secure
     begin = timer()
     user_times = []
     user_directions = []
+    start = False
+    secure = True
 
 def start_stop_callback(channel):
-    global start, begin
+    global start 
     if start:
         start = False
     else:
         start = True
+        print("---------------------")
+        print("Starting")
+        print("---------------------")
 
 def secure_insecure_callback(channel):
     global secure
@@ -71,7 +76,7 @@ def setup():
     GPIO.setup(CLK, GPIO.OUT)
     GPIO.setup(CS, GPIO.OUT)
 
-    GPIO.add_event_detect(start_stop_btn, GPIO.FALLING, callback=start_stop_callback, bouncetime=300)
+    GPIO.add_event_detect(start_stop_btn, GPIO.FALLING, callback=start_stop_callback, bouncetime=500)
     GPIO.add_event_detect(secure_btn, GPIO.FALLING, callback=secure_insecure_callback, bouncetime=300)
 
     reset()
@@ -82,99 +87,29 @@ def get_direction(start_voltage, end_voltage):
     else:
         return "L"
 
-def check_times(secure):
-    global user_times, code_times
+def check_times():
+    global user_times, code_times, secure
     if secure == False:
         user_times.sort()
         code_times.sort()
-        #TODO: sort user times, code times
-        pass
     for i in range(0, len(user_times)):
         if abs(user_times[i] - code_times[i]) > time_tolerance:
             return False 
     return True
 
-def check_positions(secure):
-    global user_directions, code_positions
+def check_positions():
+    global user_directions, code_directions, secure
     if secure == False:
         # insecure mode is not position dependent
         return True
-        pass
     for i in range(0, len(user_directions)):
-        if user_directions[i] != user_directions[i]:
+        if user_directions[i] != code_directions[i]:
             return False 
     return True
 
-def main():
-    global start, begin, end, secure
-    setup()
-
-    start_voltage = mcp.read_adc(pot_channel)
-    end_voltage = mcp.read_adc(pot_channel)
-    new_value = True
-
-    while start:
-        print("User times")
-        print("-------------------------------------------------")
-        print(user_times)
-        print("User directions")
-        print("-------------------------------------------------")
-        print(user_directions)
-        while abs(end_voltage - start_voltage) < pot_tolerance:
-            # wait while pot is not moving
-            end_voltage = mcp.read_adc(pot_channel)
-
-            ### if pot input restart time
-            end = timer()
-            new_value = False
-            time.sleep(0.3)
-#            if (end-begin) > 2:
-                # two seconds have elapsed, quit
-                # start = False
-                # break
-
-        # start timer
-        begin = timer()
-
-        new_start = mcp.read_adc(pot_channel)
-        new_end = mcp.read_adc(pot_channel)
-
-        while abs(start_voltage-new_start) > pot_tolerance:
-            # wait while pot is moving
-            new_end = mcp.read_adc(pot_channel)
-            new_value = True
-            time.sleep(0.3)
-            new_start = mcp.read_adc(pot_channel)
-            if abs(new_end-new_start) < pot_tolerance:
-                break
-            
-            #if (end-begin) > 1:
-            #    break
-    
-        # end timer
-        end = timer()
-        if new_value:
-            print("Start voltage: "+str(start_voltage))
-            print("End voltage; "+str(new_start))
-            user_times.append((end-begin)*1000)
-            print(end-begin)
-            user_directions.append(get_direction(start_voltage, end_voltage))
-            new_value = False
-            start_voltage = mcp.read_adc(pot_channel)
-            end_voltage = mcp.read_adc(pot_channel)
-        if (len(user_directions) == len(code_directions)) or len(user_directions) >= 16:
-            break
-    
-    print("User times")
-    print("-------------------------------------------------")
-    print(user_times)
-    print("User directions")
-    print("-------------------------------------------------")
-    print(user_directions)
-    print("-------------------------------------------------")
-    print("-------------------------------------------------")
-    print("-------------------------------------------------")
-    if (check_times(secure) and check_positions(secure)):
+def validate():
+    global secure
+    if (check_times() and check_positions()):
         print("Success biatch")
         GPIO.output(LED_unlock, GPIO.HIGH)
         time.sleep(2)
@@ -187,7 +122,82 @@ def main():
         GPIO.output(LED_lock, GPIO.LOW)
         #TODO play lock
         pass
-    GPIO.cleanup()
+    
+
+def main():
+    global start, begin, end, secure
+    setup()
+
+    try:
+        while True:
+            start_voltage = mcp.read_adc(pot_channel)
+            end_voltage = mcp.read_adc(pot_channel)
+            new_value = True
+            time.sleep(0.2)
+            while start:
+                begin = timer()
+                print("User times")
+                print("-------------------------------------------------")
+                print(user_times)
+                print("User directions")
+                print("-------------------------------------------------")
+                print(user_directions)
+                timeout = False
+                while abs(end_voltage - start_voltage) < pot_tolerance:
+                    # wait while pot is not moving
+                    end_voltage = mcp.read_adc(pot_channel)
+
+                    ### if pot input restart time
+                    end = timer()
+                    if (end-begin) > 4:
+                     # two seconds have elapsed, quit
+                         timeout = True
+                         break
+                    new_value = False
+                    time.sleep(0.3)
+
+                if timeout:
+                    reset()
+                    print("Timeout")
+                    break
+                begin = timer() # end button
+
+                new_start = mcp.read_adc(pot_channel)
+                new_end = mcp.read_adc(pot_channel)
+
+                while abs(start_voltage-new_start) > pot_tolerance:
+                    # wait while pot is moving
+                    new_end = mcp.read_adc(pot_channel)
+                    new_value = True
+                    time.sleep(0.3)
+                    new_start = mcp.read_adc(pot_channel)
+                    if abs(new_end-new_start) < pot_tolerance:
+                        break
+                    
+                end = timer() # end timer
+                if new_value:
+                    user_times.append((end-begin)*1000)
+                    user_directions.append(get_direction(start_voltage, end_voltage))
+                    new_value = False
+                    
+                    # reset voltages to new position
+                    start_voltage = mcp.read_adc(pot_channel)
+                    end_voltage = mcp.read_adc(pot_channel)
+                if (len(user_directions) == len(code_directions)) or len(user_directions) >= 16:
+                    print("User times")
+                    print("-------------------------------------------------")
+                    print(user_times)
+                    print("User directions")
+                    print("-------------------------------------------------")
+                    print(user_directions)
+                    print("-------------------------------------------------")
+                    print("-------------------------------------------------")
+                    print("-------------------------------------------------")
+                    validate()
+                    reset()
+                    break
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 if __name__ == "__main__":
     main()
